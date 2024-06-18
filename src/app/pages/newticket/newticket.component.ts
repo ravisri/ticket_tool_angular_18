50c894ae6ad8012426720eb493fa3dd81c58a7e0
ravig <ravi@msr-it.com>
2024-06-18 13:11:17 +0530

    working

---
+++ src/app/pages/newticket/newticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './newticket.component.html',
+  styleUrl: './newticket.component.scss'
+})
+export class NewticketComponent {
+
+}
